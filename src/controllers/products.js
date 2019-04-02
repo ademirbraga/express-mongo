5101323631350e9337a056102f27b6f5a2d44554
Ademir Braga <ademirbraga@softbox.com.br>
2019-04-02 14:58:06 -0300

    Ajustes

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,41 +1,41 @@
 class ProductsController {
-	constructor(Product) {
-		this.Product = Product;
-	};
+    constructor(Product) {
+        this.Product = Product;
+    };
 
-	get(req, res) {
-		return this.Product.find({})
-			.then(products => res.send(products))
-			.catch(err => res.status(400).send(err.message));
-	}
+    get(req, res) {
+        return this.Product.find({})
+            .then(products => res.send(products))
+            .catch(err => res.status(400).send(err.message));
+    }
 
-	getById(req, res) {
-		const { params: { id } } = req;
+    getById(req, res) {
+        const {params: {id}} = req;
 
-		return this.Product.find({ _id:id })
-			.then(products => res.send(products))
-			.catch(err => res.status(400).send(err.message));
-	}
+        return this.Product.find({_id: id})
+            .then(products => res.send(products))
+            .catch(err => res.status(400).send(err.message));
+    }
 
-	create(req, res) {
-		const product = new this.Product(req.body);
+    create(req, res) {
+        const product = new this.Product(req.body);
 
-		return product.save()
-			.then(() => res.status(201).send(product))
-			.catch(err => res.status(422).send(err.message));
-	}
+        return product.save()
+            .then(() => res.status(201).send(product))
+            .catch(err => res.status(422).send(err.message));
+    }
 
-	update(req, res) {
-		return this.Product.findOneAndUpdate({ _id: req.params.id}, req.body)
-			.then(() => res.sendStatus(200))
-			.catch(err => res.status(422).send(err.message));
-	}
+    update(req, res) {
+        return this.Product.findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(() => res.sendStatus(200))
+            .catch(err => res.status(422).send(err.message));
+    }
 
-	remove(req, res) {
-		return this.Product.remove({ _id: req.params.id})
-			.then(() => res.sendStatus(204))
-			.catch(err => res.status(400).send(err.message));
-	}
+    remove(req, res) {
+        return this.Product.remove({_id: req.params.id})
+            .then(() => res.sendStatus(204))
+            .catch(err => res.status(400).send(err.message));
+    }
 
 }
 
